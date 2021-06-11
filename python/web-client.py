be01151073329f396f1b1a7ef402ba97cd4b34ee
xugxnx <70263454+xugxnx@users.noreply.github.com>
2021-06-11 10:08:46 +0500

    minor changes

--- python/web-client.py
+++ python/web-client.py
@@ -1,4 +1,4 @@
-import http.client, time
+import http.client, time, sys
 
 target = "web-server:8080"
 
@@ -6,5 +6,6 @@ while True:
     conn = http.client.HTTPConnection(target)
     conn.request("GET", "/ping")
     resp = conn.getresponse()
-    print(resp.read().decode("utf-8"))
+    print(resp.read().decode("utf-8")),
+    sys.stdout.flush()
     time.sleep(5)
